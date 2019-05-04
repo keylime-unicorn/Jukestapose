81e0b692fc6fd1d73f71e757ddb4b13e14093d16
camicarballo <camigcarballo@gmail.com>
2019-05-04 19:48:24 -0400

    webserver: remove un-needed modules and fix server functionality

--- Jukestapose/webserver.py
+++ Jukestapose/webserver.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
-from pydub import AudioSegment
 import pyglet
 import os
 
@@ -16,20 +13,14 @@ player = pyglet.media.Player()
 def home():
 
   player.play() 
-  #print(player.playing)
-  #print(player.source)
 
   if request.method == 'POST':
-    #f = request.files['file']
-    #name = secure_filename(f.filename)
-    #f.save(os.path.join('./Music/', name))
-    #music = pyglet.media.load("./Music/" + name)
-    #player.queue(music)
-
-    name = "./Music/bensound-cute.mp3"
-    music = pyglet.media.load(name)
+    f = request.files['file']
+    name = secure_filename(f.filename)
+    f.save(os.path.join('./Music/', name))
+    music = pyglet.media.load("./Music/" + name)
     player.queue(music)
-    return "AHHHH"
+    return ""
 
   if request.method == 'GET':
     return render_template('index.html', name='index')
